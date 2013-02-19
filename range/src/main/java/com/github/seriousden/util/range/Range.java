4bd124629706829c3289fe32d6205b27e96adaf8
Denis <Demidov.D.B@Gmail.com>
2013-02-19 12:42:50 +0800

    Maven framework for good biggining.

---
+++ range/src/main/java/com/github/seriousden/util/range/Range.java
@@ -0,0 +1,9 @@
+package com.github.seriousden.util.range;
+
+/**
+ * @author Demidov Denis | Astrosoft
+ * @version 1.0
+ * @since 19.02.13
+ */
+public class Range {
+}
---
+++ src/main/java/com/github/seriousden/util/range/Range.java
@@ -0,0 +1,8 @@
+package com.github.seriousden.util.range;
+
+/**
+ * @author Denis.B.Demidov
+ * @version 1.0
+ */
+public class Range {
+}
